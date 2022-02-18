8043184612c3cc9f5992bcdbbbb63ab46479878c
iamshabell <91321698+iamshabell@users.noreply.github.com>
2022-02-18 17:43:23 +0300

    added to employee module and done setup of graphQL

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
+import { EmployeeModule } from './employee/employee.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [EmployeeModule, GraphQLModule.forRoot<ApolloDriverConfig>({
+    driver: ApolloDriver,
+    autoSchemaFile: join(process.cwd(), 'src/graphql-schema.gql'),
+  })],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
---
+++ src/employee/employee.resolver.ts
@@ -0,0 +1,9 @@
+import { Query, Resolver } from '@nestjs/graphql';
+import { Employee } from './entities/employee.entity';
+
+@Resolver(() => Employee)
+export class EmployeeResolver {
+
+    @Query(() => Employee)
+    findAll(){}
+}
